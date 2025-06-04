88097f1dfc836cc184f096e31b343c583063798b
Djbermor <deividber@hotmail.com>
2025-06-03 23:33:56 -0500

    feat: proyecto finalizado

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-process.env.VITE_API_URL = 'http://localhost:4000'
-process.env.VITE_WOMPI_PUBLIC_KEY = 'test_pusblic_key'
-process.env.VITE_WOMPI_API_URL = 'https://sandbox.wompi.co/v1'
--- src/application/fetchProducts.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from '@domain/product/entities/Product'
-import { getProducts } from '@infra/api/wompiApi'
-
-export async function fetchProducts(): Promise<Product[]> {
-  const response = await getProducts()
-  return response.data
-}
